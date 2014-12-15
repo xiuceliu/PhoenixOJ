5526bb204f43172ce7cce4d1dda8e9bc73d46a27
woshilalala <pkqs90@gmail.com>
2014-12-15 17:14:29 +0800

    news 40%

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -12,14 +12,17 @@ class NewsController < ApplicationController
 	def edit
 		@news = News.find(params[:id])
 	end
-	def new
-  end
 	def create
 		@news = News.new(params[:news])
 		@news.user = current_user
 		@news.save
 		redirect_to news_index_path
 	end
+	def update
+    @news = News.find(params[:id])
+    @news.update_attributes(params[:news])
+    redirect_to news_index_path
+  end
 	def destroy
     	@problem = Problem.find(params[:problem_id])
     	@discuss = @problem.discusses.find(params[:id])
